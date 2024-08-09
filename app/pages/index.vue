8b418854a8fff45300a6c685c538fdb3f55f39a5
rafaeldellaquila <rafael@dellaquila.dev>
2024-08-09 11:55:48 -0300

    :mag: put the seo data meta

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,31 @@
 <template>
   <h1>Hello World Index</h1>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'Rafael DellAquila - Developer',
+  description: 'Nuxt portfolio to learn the framework',
+  ogTitle: 'Rafael DellAquila - Developer',
+  ogDescription: 'Nuxt portfolio to learn the framework',
+  ogImage: 'https://dellaquila.dev/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2Favatar.8d47287a86e9ea34a08932cdb6720813.jpg&w=384&q=75',
+  ogUrl: 'https://dellaquila.dev',
+  twitterTitle: 'Rafael DellAquila - Developer',
+  twitterDescription: 'Nuxt portfolio to learn the framework',
+  twitterImage: 'https://dellaquila.dev/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2Favatar.8d47287a86e9ea34a08932cdb6720813.jpg&w=384&q=75',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
