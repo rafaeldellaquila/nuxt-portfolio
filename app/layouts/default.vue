4e298c1c4d4475e792b2090bdaf67d174225271f
rafaeldellaquila <rafael@dellaquila.dev>
2024-08-09 11:55:11 -0300

    :lipstick: extract nav to a component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,19 +2,7 @@
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
       <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">dellaquila.dev</NuxtLink>
-      <nav class="font-mono">
-        <ul class="flex space-x-4">
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/blog">Blog</NuxtLink>
-          </li>
-        </ul>
-      </nav>
+      <Menu />
     </header>
     <main class="p-2">
       <slot />
